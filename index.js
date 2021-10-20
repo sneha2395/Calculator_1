69c6b87ef47a90d79cc0115afe874d3911385137
sneha2395 <agarwal.sneha.231995@gmail.com>
2021-10-20 21:24:21 +0530

    functionalit of the calculator is done in index.js file

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-let screeen= document.getElementById('screen');
+let screen= document.getElementById('screen');
 buttons = document.querySelectorAll('button');
-for(items of buttons){
+let screenValue = "";
+for(item of buttons){
     item.addEventListener('click', (e)=>{
-        buttonText = e.target.innertext;
-        console.log('Button text is', buttontext);
-        if(buttonText == 'X'){
-            buttonText ='*';
-            screen.value += buttonText;
+        buttonText = e.target.innerText;
+        console.log('Button text is', buttonText);
+        if(buttonText== 'X'){
+            buttonText = '*';                   
+            screenValue += buttonText;
             screen.value = screenValue;
 
         }
         else if (buttonText == 'C'){
-        screenValue = "=";
-        screen.value = screenValue;
+            screenValue = "";
+            screen.value = screenValue;
         }
-        else if (buttonText =='='){
+      else if (buttonText == '='){
             screen.value = eval(screenValue);
         }
         else{
             screenValue += buttonText;
             screen.value = screenValue;
         }
-
+       
     })
 
 }
